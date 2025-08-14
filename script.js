cbbd4e19f7838a55f5c7482cbfd810efc072ca46
Turner Cortese <turner.cortese@yahoo.com>
2025-08-14 14:41:30 -0400

    created product properties class with constructor

---
+++ script.js
@@ -0,0 +1,11 @@
+class ProductProperties{
+    // constructor method to set properties
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    
+
+
+}
