c234197db91d00daeb7a15f3847cacbb15616d5c
Turner Cortese <turner.cortese@yahoo.com>
2025-08-14 15:05:11 -0400

    override toString and added expiration date

--- script.js
+++ script.js
@@ -21,7 +21,10 @@ class PerishableProductProperties extends ProductProperties {
     constructor(name, price, quantity, expirationDate) {
         super(name, price, quantity);
         this.expirationDate = expirationDate;
+    }
 
+    toString () {
+        console.log(`Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`);
     }
 
 }
